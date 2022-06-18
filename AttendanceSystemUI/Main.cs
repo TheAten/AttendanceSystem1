29c0a441fcd19853b86f6702988e309820d0f100
krist <krist@SNUPIX-PC>
2022-06-18 09:51:50 +0200

    Add project files.

---
+++ AttendanceSystemUI/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AttendanceSystemUI
+{
+    public partial class Main : MetroFramework.Forms.MetroForm
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
